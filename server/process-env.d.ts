37d15410cc9a57ffa58b47507bde15c16138ba82
Sakui <63701743+Sakui-ph@users.noreply.github.com>
2024-02-04 18:43:15 +0800

    add: env

---
+++ server/process-env.d.ts
@@ -0,0 +1,18 @@
+declare global {
+    namespace NodeJS {
+      interface ProcessEnv {
+        STATUS: 'dev' | 'prod';
+        DEV_PORT: string;
+        PROD_PORT: string;
+        DB_HOST: string;
+        DB_USER: string;
+        DB_PORT: string;
+        DB_PASSWORD: string;
+        DB_DIALECT: string;
+      }
+    }
+  }
+  
+  // If this file has no import/export statements (i.e. is a script)
+  // convert it into a module by adding an empty export statement.
+  export {}
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,17 @@
 import express, { Application } from 'express';
 import { UserRoutes } from './routes/user';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 const app: Application = express();
-const port = 5000;
-
 app.use('/user', UserRoutes);
 
+let PORT : string | undefined;
+process.env.STATUS === 'dev'
+? PORT = process.env.DEV_PORT
+: PORT = process.env.PROD_PORT;
 
-app.listen(port, () => {
-    console.log(`Server running on port ${port}`)
+app.listen(PORT, () => {
+    console.log(`Server running in ${process.env.STATUS} mode on port ${PORT}`)
 })

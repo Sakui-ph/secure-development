474510f0a5432ddd8b56020039db93cbedd04116
kalaw410 <stacy_selena_kalaw@dlsu.edu.ph>
2024-02-13 19:55:21 +0800

    Added sample insert for db

--- client/src/components/server.js
+++ client/src/components/server.js
@@ -1,10 +1,14 @@
+// npm install express body-parser cors mysql2
+// cd client/src/components
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const mysql = require('mysql2');
 
 const app = express();
-const port = 3000;
+const port = 3100;
+
+let counter = 0;
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -17,28 +21,42 @@ const db = mysql.createConnection({
    database: 'cssecdv',
 });
 
-db.connect((err) => {
+function generatePrefixID() {
+   counter++;
+   return counter.toString().padStart(2, '0');
+}
+
+/*db.connect((err) => {
     if (err) {
        console.error('MySQL connection error:', err);
     } else {
        console.log('Connected to MySQL database');
     }
- });
+ });*/
+
+ // Test insert
+/*db.connect(function(err) {
+   if (err) throw err;
+   console.log("Connected!");
+   var sql = "INSERT INTO users (prefix_id, email, phone_number, password, full_name) VALUES ('001', 'email@gmail.com', '09458991485', '123', 'John Doe')";
+   db.query(sql, function (err, result) {
+      if (err) {
+         console.error('Error inserting data into the database:', err);
+         throw err;
+      }
+      console.log("1 record inserted");
+   });
+});*/
 
 // Handle user registration
-/*app.post('/register', (req, res) => {
-    const { username, first_name, last_name, email, country, phone_number, password } = req.body;
- 
-    const sql = 'INSERT INTO users (username, first_name, last_name, email, country, phone_number, password) VALUES (?, ?, ?, ?, ?, ?, ?, ?)';
-    db.query(sql, [username, first_name, last_name, email, country, phone_number, password], (err, result) => {
-       if (err) {
-          console.error('MySQL query error:', err);
-          res.status(500).json({ success: false, error: 'Internal server error' });
-       } else {
-          res.status(200).json({ success: true, message: 'User registered successfully' });
-       }
-    });
- });*/
+app.post('/', (req, res) => {
+    const prefix_id = generatePrefixID();
+    const full_name = req.body.name;
+    const email = req.body.email;
+    const phone_number = req.body.number;
+    const password = req.body.password;
+    const profile_photo = req.body.profileImage;
+ });
  
 
  app.listen(port, () => {

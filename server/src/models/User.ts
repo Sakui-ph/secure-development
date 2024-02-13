c39c5d32b5b2a0024972f80910c3b1243662721a
kalaw410 <stacy_selena_kalaw@dlsu.edu.ph>
2024-02-13 20:03:27 +0800

    Updated database model

--- server/src/models/User.ts
+++ server/src/models/User.ts
@@ -3,13 +3,14 @@ export default interface User {
     readonly prefix_id: number; // 000 for admin, 001 for user ig
     //readonly id: string; // (7) 7 digit unique id INT
 
-    username: string; // varchar(30)
-    first_name: string; // varchar(50)
-    last_name: string; // varchar(50)
+    //username: string; // varchar(30)
+    //first_name: string; // varchar(50)
+    //last_name: string; // varchar(50)
+    full_name: string //varchar(255)
 
     email: string; // varchar(62)
     password: string; // varchar(50)
-    country: string; // varchar(2)
+    //country: string; // varchar(2)
     phone_number: string; // varchar(25)
     /* From stackoverflow:
     All as varchar (they aren't numbers but "collections of digits")

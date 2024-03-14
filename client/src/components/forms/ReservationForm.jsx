2336535c60c723cc4406379c1d2c40d2366897aa
alyssaysabelle <85222862+alyssaysabelle@users.noreply.github.com>
2024-03-14 21:04:44 +0800

    update reservation form

--- client/src/components/forms/ReservationForm.jsx
+++ client/src/components/forms/ReservationForm.jsx
@@ -1,39 +1,78 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
-export default function ReservationForm () {
-    const [email, setEmail] = useState('');
-    const [date, setDate] = useState('');
-    const [time, setTime] = useState('');
-    
-    const handleDateChange = (e) => {
-        setDate(e.target.value);
-    };
-    
-    const handleTimeChange = (e) => {
-        setTime(e.target.value);
+export default function ReservationForm() {
+    const [formFields, setFormFields] = useState({
+        date: '',
+        time: '',
+        email: '',
+    });
+
+    const [submitting, setSubmitting] = useState(false);
+
+    const handleInputChange = (e) => {
+        setFormFields({ ...formFields, [e.target.name]: e.target.value });
     };
 
-    const handleEmailChange = (e) => {
-        setEmail(e.target.value);
-    };
-    
     const handleSubmit = (e) => {
         e.preventDefault();
+        setSubmitting(true);
     };
-    
+
+    useEffect(() => {
+        if (submitting) {
+            /* Login(inputFields.email, inputFields.password).then((result) => {
+                setSubmitting(false);
+                if (result) window.location.href = '/home';
+            }); */
+            console.log('Form submitted:', formFields);
+
+            // Reset the form after submission
+            setFormFields({
+                date: '',
+                time: '',
+                email: '',
+            });
+
+            setSubmitting(false);
+        }
+    }, [submitting, formFields]);
+
     return (
-    <form onSubmit={handleSubmit}>
-        <h1>Reservation Form</h1>
-        <label>Email
-            <input type="email" value={email} onChange={handleEmailChange} />
-        </label>
-        <label>Date
-            <input type="date" value={date} onChange={handleDateChange} />
-        </label>
-        <label>Time
-            <input type="time" value={time} onChange={handleTimeChange} />
-        </label>
-        <button type="submit">Submit</button>
+        <form className="form">
+            <label htmlFor="name">Date:</label>
+            <input
+                className="input"
+                type="date"
+                id="date"
+                name="date"
+                value={formFields.date}
+                onChange={handleInputChange}
+                required
+            />
+
+            <label htmlFor="time">Time:</label>
+            <input
+                className="input"
+                type="time"
+                id="time"
+                name="time"
+                value={formFields.phone}
+                onChange={handleInputChange}
+                required
+            />
+
+            <label htmlFor="email">Email:</label>
+            <input
+                className="input"
+                type="email"
+                id="email"
+                name="email"
+                value={formFields.email}
+                onChange={handleInputChange}
+                required
+            />
+
+            <button type="submit">Submit</button>
         </form>
     );
-};
+}

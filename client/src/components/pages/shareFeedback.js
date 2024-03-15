e5c65dc5a8def8a37f881f345194157931597852
kalaw410 <stacy_selena_kalaw@dlsu.edu.ph>
2024-03-15 11:07:44 +0800

    create share feedback.js

---
+++ client/src/components/pages/shareFeedback.js
@@ -0,0 +1,7 @@
+import React, { useState } from 'react';
+import FeedbackForm from '../forms/FeedbackForm';
+import { LogError } from '../../utils/error-handlers/error-logger';
+
+export default function InquiriesPage() {
+    return <FeedbackForm />;
+}

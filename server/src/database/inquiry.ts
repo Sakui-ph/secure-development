75cb0f97ebe0e6879b541d81a341b1ef95717b49
kalaw410 <stacy_selena_kalaw@dlsu.edu.ph>
2024-03-17 22:24:05 +0800

    create inquiry database

---
+++ server/src/database/inquiry.ts
@@ -0,0 +1,20 @@
+import db from './db';
+import { buildUpdateQuery, convertSearchByToString } from '../utils/dbHelpers';
+import { Inquiry } from '../models/Inquiry';
+
+class InquiryDB {
+    create = async (inquiry: Inquiry) => {
+        const query =
+            'INSERT INTO inquiries (name, phone, email, concern) ';
+        const values: string = `VALUES 
+        ('${inquiry.name}', 
+        '${inquiry.phone}', 
+        '${inquiry.email}', 
+        '${inquiry.concern}')`;
+
+        const result = await db.queryDatabase(query + values, []);
+        return result[0][0];
+    };
+}
+
+export default new InquiryDB();

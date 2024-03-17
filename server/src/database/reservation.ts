bf4e40a5b158951ea2c7795589a3bbe4361a5307
kalaw410 <stacy_selena_kalaw@dlsu.edu.ph>
2024-03-17 22:42:04 +0800

    create reservation database

---
+++ server/src/database/reservation.ts
@@ -0,0 +1,38 @@
+import db from './db';
+import { buildUpdateQuery, convertSearchByToString } from '../utils/dbHelpers';
+import { Reservation } from '../models/Reservation';
+
+class ReservationDB {
+    find = async (projection: string[], searchBy: Record<string, any>) => {
+        const searchParams = convertSearchByToString(searchBy);
+        const result = await db.queryDatabase(
+            `SELECT ${projection.join(', ')} FROM reservations WHERE ${searchParams}`,
+            [],
+        );
+
+        return result[0][0];
+    };
+
+    create = async (reservation: Reservation) => {
+        const query =
+            'INSERT INTO reservations (date, time, email, room) ';
+        const values = `VALUES 
+        ('${reservation.date}', 
+        '${reservation.time}', 
+        '${reservation.email}', 
+        '${reservation.room}')`;
+
+        const result = await db.queryDatabase(query + values, []);
+        return result[0][0];
+    };
+
+    delete = async (searchBy: Record<string, any>) => {
+        const searchParams = convertSearchByToString(searchBy);
+        const query = `DELETE FROM reservations WHERE ${searchParams}`;
+
+        const result = await db.queryDatabase(query, []);
+        return result[0][0];
+    };
+}
+
+export default new ReservationDB();

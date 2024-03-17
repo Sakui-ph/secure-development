8a7bf4f71d1cccac5bbd41b184511e9e1eb9b9af
kalaw410 <stacy_selena_kalaw@dlsu.edu.ph>
2024-03-17 22:24:15 +0800

    create inquiry model

---
+++ server/src/models/Inquiry.ts
@@ -0,0 +1,15 @@
+export interface Inquiry {
+    readonly id?: string;
+    name: string;
+    phone: string;
+    email: string; 
+    concern: string;
+}
+
+export const InquiryParams = {
+    ID: 'id',
+    NAME: 'name',
+    PHONE: 'phone',
+    EMAIL: 'email',
+    CONCERN: 'concern',
+};

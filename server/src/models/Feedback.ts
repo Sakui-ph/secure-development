0ffb9a587eeb655d61e77a4c3feacf68f5e30581
kalaw410 <stacy_selena_kalaw@dlsu.edu.ph>
2024-03-17 22:24:38 +0800

    create feedback model

---
+++ server/src/models/Feedback.ts
@@ -0,0 +1,13 @@
+export interface Feedback {
+    readonly id?: string;
+    author: string; 
+    comment: string;
+    photo: File; 
+}
+
+export const FeedbackParams = {
+    ID: 'id',
+    AUTHOR: 'author',
+    COMMENT: 'comment',
+    PHOTO: 'photo',
+};

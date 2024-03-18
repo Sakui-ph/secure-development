b1dbf78f368ad46f6d5957ef49c340943b9f199a
Richard <94967357+lcdApple@users.noreply.github.com>
2024-03-18 15:09:52 +0800

    Create adminCheckInquiry.js

---
+++ client/src/components/pages/adminCheckInquiry.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import AdminCheckInquiryForm from '../forms/AdminCheckInquiryForm';
+
+export default function AdminCheckInquiryForm() {
+    return (
+        <div>
+            <AdminCheckInquiryForm />
+        </div>
+    );
+}

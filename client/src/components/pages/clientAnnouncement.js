97739d8a2f7cabcdc4f03246f86b8afbfec49c4c
kalaw410 <stacy_selena_kalaw@dlsu.edu.ph>
2024-03-20 12:12:10 +0800

    create client view announcement page

---
+++ client/src/components/pages/clientAnnouncement.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ClientViewAnnouncementForm from '../forms/ClientViewAnnouncementForm';
+
+export default function ClientViewAnnouncementPage() {
+    return (
+        <div>
+            <h1>View Announcement</h1>
+            <ClientViewAnnouncementForm />
+        </div>
+    );
+}

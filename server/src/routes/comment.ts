0292ff60dfb954eed4fafdf2cc985d84660af3d0
kalaw410 <stacy_selena_kalaw@dlsu.edu.ph>
2024-03-20 12:39:32 +0800

    create comment route

---
+++ server/src/routes/comment.ts
@@ -0,0 +1,69 @@
+import express, { Request, Response } from 'express';
+import commentController from '../controller/comment'; // Import your comment controller
+import { LogError, LogInfo, LogType } from '../utils/logger';
+import asyncify from 'express-asyncify';
+import bodyParser from 'body-parser';
+import { uploadFormdata } from '../utils/multerHandler';
+
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
+const extendedParser = bodyParser.urlencoded({ extended: true });
+
+const router = asyncify(express.Router());
+
+router.post(
+    '/create',
+    express.json(),
+    extendedParser,
+    uploadFormdata,
+    async (req: Request, res: Response) => {
+        try {
+            const result = await commentController.createComment(req, res);
+            if (!result) {
+                LogInfo(`Comment created successfully!`, LogType.TRANSACTION);
+                res.send('Comment created successfully!').status(200);
+            } else {
+                LogError('Error creating comment', result, LogType.TRANSACTION);
+                res.send('Error creating comment').status(500);
+            }
+        } catch (e) {
+            LogError('Error creating comment', e as Error, LogType.TRANSACTION);
+            res.send('Error creating comment').status(500);
+        }
+    },
+);
+
+// Find all comments route
+router.get(
+    '/find-all',
+    async (req: Request, res: Response) => {
+        try {
+            const comments = await commentController.getComment();
+            res.send(comments).status(200);
+        } catch (e) {
+            LogError('Error finding comments', e as Error, LogType.TRANSACTION);
+            res.send('Error finding comments').status(500);
+        }
+    },
+);
+
+router.delete(
+    '/delete/:id',
+    async (req: Request, res: Response) => {
+        try {
+            const id = req.params.id;
+            const result = await commentController.deleteComment(id);
+            if (!result) {
+                LogInfo(`Comment deleted successfully!`, LogType.TRANSACTION);
+                res.send('Comment deleted successfully!').status(200);
+            } else {
+                LogError('Error deleting comment', result, LogType.TRANSACTION);
+                res.send('Error deleting comment').status(500);
+            }
+        } catch (e) {
+            LogError('Error deleting comment', e as Error, LogType.TRANSACTION);
+            res.send('Error deleting comment').status(500);
+        }
+    },
+);
+
+export { router as CommentRoutes };

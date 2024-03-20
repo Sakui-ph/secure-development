73d4d9d0bb98643c8e6d573b97d9b5516076e841
kalaw410 <stacy_selena_kalaw@dlsu.edu.ph>
2024-03-20 12:39:24 +0800

    create comment controller

---
+++ server/src/controller/comment.ts
@@ -0,0 +1,59 @@
+import { Request, Response } from 'express';
+import { Comment } from '../models/Comment';
+import CommentDB from '../database/comment';
+import { LogError, LogType, LogWarning } from '../utils/logger';
+
+module.exports = {
+    createComment: async (req: Request, res: Response): Promise<any> => {
+        const newComment: Comment = {
+            author: req.body.author,
+            text: req.body.text,
+            date: new Date(),
+        };
+        try {
+            const result = await CommentDB.create(newComment);
+            res.send(result).status(200);
+        } catch (e) {
+            if (e instanceof Error) {
+                LogError('Error creating comment', e, LogType.TRANSACTION);
+            }
+            res.status(500).send('Error creating comment');
+        }
+    },
+
+    getComment: async (req: Request, res: Response): Promise<any> => {
+        try {
+            const allComments = await CommentDB.find();
+            res.send(allComments).status(200);
+        } catch (e) {
+            if (e instanceof Error) {
+                LogError('Error fetching all comments', e, LogType.TRANSACTION);
+            }
+            res.status(500).send('Error fetching all comments');
+        }
+    },
+
+    deleteComment: (searchBy: string[]) => {
+        return async (req: Request, res: Response): Promise<any> => {
+            const searchObject: Record<string, any> = {};
+
+            searchBy.forEach((key) => {
+                if (req.body[key] !== undefined) {
+                    searchObject[key] = req.body[key];
+                }
+            });
+
+            try {
+                const result = await CommentDB.delete(searchObject);
+                res.send(result).status(200);
+            } catch (e) {
+                if (e instanceof Error && e.stack !== undefined) {
+                    //LogError(e.stack, LogType.TRANSACTION);
+                }
+                res.status(500).send('Error deleting comment');
+            }
+        };
+    },
+};
+
+export default module.exports;
